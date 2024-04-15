7d4aa1c675cf1580a1d722e1aa2cfbda7c710c6f
Override <112493528+0V3RR1DE0@users.noreply.github.com>
2024-04-15 16:56:06 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,115 +1,169 @@
+window.addEventListener('load', function() {
+    // Reset all selected settings and clear the script preview
+    document.getElementById('language-options').selectedIndex = 0;
+    document.getElementById('command-options').selectedIndex = 0;
+    document.getElementById('script-preview').value = '';
+    document.getElementById('output-to-text-file').checked = false;
+});
+
 // An object of your command options for each language
 var commands = {
-    "bash-linux": [
-        "mkdir directory_name",
-        "rmdir directory_name",
-        "touch filename.txt",
-        "rm filename.txt",
-        "cp source_file destination_file",
-        "mv old_filename new_filename",
-        "cat filename.txt",
-        "echo 'text to append' >> filename.txt",
-        "grep 'pattern' filename.txt",
-        "wc -l filename.txt",
-        "sort filename.txt",
-        "uniq filename.txt",
-        "sed 's/find/replace/g' filename.txt",
-        "awk '{print $1}' filename.txt",
-        "split -l 100 filename.txt",
-        "uname -a",
-        "df -h",
-        "free -h",
-        "uptime",
-        "ifconfig",
-        "route -n",
-        "ping google.com",
-        "netstat -tuln",
-        "ps aux",
-        "kill -9 process_id",
-        "command &",
-        "kill %job_number",
-        "who",
-        "passwd username",
-        "useradd username",
-        "userdel username",
-        "if [ condition ]; then\n    # commands\nfi",
-        "if [ condition ]; then\n    # commands\nelse\n    # commands\nfi",
-        "case $variable in\n    pattern1)\n        # commands\n        ;;\n    pattern2)\n        # commands\n        ;;\n    *)\n        # default commands\n        ;;\nesac",
-        "for i in {1..10}; do\n    # commands\ndone",
-        "while [ condition ]; do\n    # commands\ndone",
-        "function_name() {\n    # commands\n}",
-        "echo 'Hello, World!'",
-        "sleep 5",
-        "exit 0"
-    ],
-    "bash-windows": [
-        "mkdir directory_name",
-        "rmdir directory_name",
-        "echo. > filename.txt",
-        "del filename.txt",
-        "copy source_file destination_file",
-        "move old_filename new_filename",
-        "type filename.txt",
-        "echo text to append >> filename.txt",
-        "findstr 'pattern' filename.txt",
-        "find /c /v \"\" < filename.txt",
-        "sort filename.txt",
-        "sort /unique filename.txt",
-        "sed 's/find/replace/g' filename.txt",
-        "for /f \"tokens=1\" %i in (filename.txt) do echo %i",
-        "split -l 100 filename.txt",
-        "ver",
-        "dir",
-        "systeminfo",
-        "netstat -an",
-        "tasklist",
-        "taskkill /f /pid process_id",
-        "start command",
-        "taskkill /f /pid job_number",
-        "query user",
-        "net user username *",
-        "net user username /add",
-        "net user username /delete",
-        "if condition (\n    :: commands\n) else (\n    :: commands\n)",
-        "for /l %i in (1,1,10) do (\n    :: commands\n)",
-        "set /p variable=Enter variable: \nswitch %variable% (\n    case value1 (\n        :: commands\n        exit /b\n    )\n    case value2 (\n        :: commands\n        exit /b\n    )\n    default (\n        :: default commands\n        exit /b\n    )\n)",
-        "echo Hello, World!",
-        "timeout /t 5",
-        "exit /b 0"
-    ],
-    "powershell": [
-        "New-Item -ItemType Directory -Name directory_name",
-        "Remove-Item directory_name",
-        "New-Item -ItemType File -Name filename.txt",
-        "Remove-Item filename.txt",
-        "Copy-Item source_file destination_file",
-        "Rename-Item old_filename new_filename",
-        "Get-Content filename.txt",
-        "Add-Content filename.txt 'text to append'",
-        "Select-String -Pattern 'pattern' filename.txt",
-        "(Get-Content filename.txt | Measure-Object -Line).Lines",
-        "Sort-Object filename.txt",
-        "Get-Unique filename.txt",
-        "(Get-Content filename.txt) -replace 'find','replace' | Set-Content filename.txt",
-        "Get-Content filename.txt | ForEach-Object { $_.Split(' ')[0] }",
-        "Get-Content filename.txt | Out-File -Encoding ascii -LiteralPath filename.txt -MaxLines 100",
-        "$PSVersionTable",
-        "Get-Volume",
-        "Get-Process",
-        "Stop-Process -ID process_id -Force",
-        "Start-Job -ScriptBlock { command }",
-        "Stop-Job -ID job_number",
-        "Get-WmiObject -Class Win32_ComputerSystem",
-        "Set-LocalUser -Name username -Password (ConvertTo-SecureString 'password' -AsPlainText -Force)",
-        "New-LocalUser -Name username -Password (ConvertTo-SecureString 'password' -AsPlainText -Force)",
-        "Remove-LocalUser -Name username",
-        "if (condition) {\n    # commands\n} else {\n    # commands\n}",
-        "for ($i=1; $i -le 10; $i++) {\n    # commands\n}",
-        "switch ($variable) {\n    value1 {\n        # commands\n        break\n    }\n    value2 {\n        # commands\n        break\n    }\n    default {\n        # default commands\n    }\n}",
-        "Write-Host 'Hello, World!'",
-        "Start-Sleep -Seconds 5",
-        "exit 0"
-    ]
+    "bash-linux": {
+        "File and Directory Operations": [
+            "mkdir directory_name",
+            "rmdir directory_name",
+            "touch filename.txt",
+            "rm filename.txt",
+            "cp source_file destination_file",
+            "mv old_filename new_filename"
+        ],
+        "File Content Operations": [
+            "cat filename.txt",
+            "echo 'text to append' >> filename.txt",
+            "grep 'pattern' filename.txt",
+            "wc -l filename.txt",
+            "sort filename.txt",
+            "uniq filename.txt",
+            "sed 's/find/replace/g' filename.txt",
+            "awk '{print $1}' filename.txt",
+            "split -l 100 filename.txt"
+        ],
+        "Text Processing": [
+            "uname -a",
+            "df -h",
+            "free -h",
+            "uptime",
+            "ifconfig",
+            "route -n",
+            "ping google.com",
+            "netstat -tuln"
+        ],
+        "System Information": [
+            "ps aux",
+            "kill -9 process_id",
+            "command &",
+            "kill %job_number",
+            "who"
+        ],
+        "User Management": [
+            "passwd username",
+            "useradd username",
+            "userdel username"
+        ],
+        "Conditional Statements": [
+            "if [ condition ]; then\n    # commands\nfi",
+            "if [ condition ]; then\n    # commands\nelse\n    # commands\nfi",
+            "case $variable in\n    pattern1)\n        # commands\n        ;;\n    pattern2)\n        # commands\n        ;;\n    *)\n        # default commands\n        ;;\nesac"
+        ],
+        "Loops": [
+            "for i in {1..10}; do\n    # commands\ndone",
+            "while [ condition ]; do\n    # commands\ndone"
+        ],
+        "Functions": [
+            "function_name() {\n    # commands\n}"
+        ],
+        "Miscellaneous": [
+            "echo 'Hello, World!'",
+            "sleep 5",
+            "exit 0"
+        ]
+    },
+    "bash-windows": {
+        "File and Directory Operations": [
+            "mkdir directory_name",
+            "rmdir directory_name",
+            "echo. > filename.txt",
+            "del filename.txt",
+            "copy source_file destination_file",
+            "move old_filename new_filename"
+        ],
+        "File Content Operations": [
+            "type filename.txt",
+            "echo text to append >> filename.txt",
+            "findstr 'pattern' filename.txt",
+            "find /c /v \"\" < filename.txt",
+            "sort filename.txt",
+            "sort /unique filename.txt",
+            "sed 's/find/replace/g' filename.txt",
+            "for /f \"tokens=1\" %i in (filename.txt) do echo %i",
+            "split -l 100 filename.txt"
+        ],
+        "Text Processing": [
+            "ver",
+            "dir",
+            "systeminfo",
+            "netstat -an"
+        ],
+        "System Information": [
+            "tasklist",
+            "taskkill /f /pid process_id",
+            "start command",
+            "taskkill /f /pid job_number",
+            "query user"
+        ],
+        "User Management": [
+            "net user username *",
+            "net user username /add",
+            "net user username /delete"
+        ],
+        "Conditional Statements": [
+            "if condition (\n    :: commands\n) else (\n    :: commands\n)",
+            "for /l %i in (1,1,10) do (\n    :: commands\n)",
+            "set /p variable=Enter variable: \nswitch %variable% (\n    case value1 (\n        :: commands\n        exit /b\n    )\n    case value2 (\n        :: commands\n        exit /b\n    )\n    default (\n        :: default commands\n        exit /b\n    )\n)"
+        ],
+        "Miscellaneous": [
+            "echo Hello, World!",
+            "timeout /t 5",
+            "exit /b 0"
+        ]
+    },
+    "powershell": {
+        "File and Directory Operations": [
+            "New-Item -ItemType Directory -Name directory_name",
+            "Remove-Item directory_name",
+            "New-Item -ItemType File -Name filename.txt",
+            "Remove-Item filename.txt",
+            "Copy-Item source_file destination_file",
+            "Rename-Item old_filename new_filename"
+        ],
+        "File Content Operations": [
+            "Get-Content filename.txt",
+            "Add-Content filename.txt 'text to append'",
+            "Select-String -Pattern 'pattern' filename.txt",
+            "(Get-Content filename.txt | Measure-Object -Line).Lines",
+            "Sort-Object filename.txt",
+            "Get-Unique filename.txt",
+            "(Get-Content filename.txt) -replace 'find','replace' | Set-Content filename.txt",
+            "Get-Content filename.txt | ForEach-Object { $_.Split(' ')[0] }",
+            "Get-Content filename.txt | Out-File -Encoding ascii -LiteralPath filename.txt -MaxLines 100"
+        ],
+        "System Information": [
+            "$PSVersionTable",
+            "Get-Volume",
+            "Get-Process",
+            "Stop-Process -ID process_id -Force"
+        ],
+        "Process Management": [
+            "Start-Job -ScriptBlock { command }",
+            "Stop-Job -ID job_number",
+            "Get-WmiObject -Class Win32_ComputerSystem"
+        ],
+        "User Management": [
+            "Set-LocalUser -Name username -Password (ConvertTo-SecureString 'password' -AsPlainText -Force)",
+            "New-LocalUser -Name username -Password (ConvertTo-SecureString 'password' -AsPlainText -Force)",
+            "Remove-LocalUser -Name username"
+        ],
+        "Conditional Statements": [
+            "if (condition) {\n    # commands\n} else {\n    # commands\n}",
+            "for ($i=1; $i -le 10; $i++) {\n    # commands\n}",
+            "switch ($variable) {\n    value1 {\n        # commands\n        break\n    }\n    value2 {\n        # commands\n        break\n    }\n    default {\n        # default commands\n    }\n}"
+        ],
+        "Miscellaneous": [
+            "Write-Host 'Hello, World!'",
+            "Start-Sleep -Seconds 5",
+            "exit 0"
+        ]
+    }
 };
 
 // Populate the command options dropdown when a language is selected
@@ -117,10 +171,15 @@ document.getElementById('language-options').addEventListener('change', function(
     var language = this.value;
     var select = document.getElementById('command-options');
     select.innerHTML = ""; // Clear the current options
-    for(var i = 0; i < commands[language].length; i++) {
-        var option = document.createElement('option');
-        option.text = commands[language][i];
-        select.add(option);
+    for(var category in commands[language]) {
+        var optgroup = document.createElement('optgroup');
+        optgroup.label = category;
+        for(var i = 0; i < commands[language][category].length; i++) {
+            var option = document.createElement('option');
+            option.text = commands[language][category][i];
+            optgroup.appendChild(option);
+        }
+        select.appendChild(optgroup);
     }
 });
 
